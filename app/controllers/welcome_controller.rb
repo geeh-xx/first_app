d8c08a17e4cc81302c7dba574294fc4eb199cf89
Geeh <geeh@fumec.edu.br>
2013-06-12 21:47:21 -0300

    layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,14 @@
 class WelcomeController < ApplicationController
 
+	layout 'application_black' , :only => :black
+
 	def index
 	end
 
+	def black
+	#	render :layout => 'application_black'
+	end
+
+	def only
+	end
 end
